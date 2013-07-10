1cbf4672a1c4deb04d17c17bd0041b06dc71a21c
Evan Chen <chen.evan6@gmail.com>
2013-07-09 22:47:37 -0700

    Moved code under a main method; re-organize

--- compute.py
+++ compute.py
@@ -32,34 +32,33 @@ def getVars(color):
 
 
 class Vertex:
-	def __init__(self, i):
+	def __init__(self, i, color):
 		self.name = i
+		self.color = color
 	def __repr__(self):
 		return "v_%d" %(self.name)
 	up = None
 	down = None
 	component = None
-	@property
-	def color(self):
-		return letters[self.name]
 	def __eq__(self, other):
 		if other is None: return False
 		return self.name == other.name
-	def __lt__(self, other):
-		return self.name < other.name
-	def __gt__(self, other):
-		return self.name > other.name
+	def __lt__(self, other): return self.name < other.name
+	def __gt__(self, other): return self.name > other.name
+	def __le__(self, other): return self.name <= other.name
+	def __ge__(self, other): return self.name >= other.name
 
 class Component:
 	def __init__(self, number, start):
 		self.name = number
 		self.breakpoints = [start]
+		self.members = []
 		self.pockets = []
 	def __repr__(self):
 		return "c_%s = %s" %(self.name, self.breakpoints)
 	@property
 	def color(self):
-		return letters[self.breakpoints[0].name]
+		return self.breakpoints[0].color
 	def addBreakpoint(self, vertex):
 		self.breakpoints.append(vertex)
 		self.breakpoints.sort()
@@ -119,7 +118,7 @@ class Pocket:
 
 		if target.name <= start.name or target.name >= end.name: return False # another sanity check
 
-		checkpoints = [v for v in vertices[start.name:end.name+1] if v.component == self.parent] # points of the loop
+		checkpoints = [v for v in self.parent.members if start <= v and v <= end] # points of the loop
 		if len(checkpoints) == 2: return True # loops are always good if the earlier sanity check returned OK
 
 		switchpoints = [start] # points where direction changes
@@ -149,70 +148,7 @@ class Pocket:
 				return (-coeff) * monomial / diff * same + (diff + coeff * same) * self.breakPolynomial(monomial / diff)
 		return 0
 
-
-# Get input {{{1
-f = sys.stdin
-letters = f.readline().strip()
-n = len(letters)
-bit1 = f.readline().strip()
-bit2 = f.readline().strip()
-assert len(bit1) == n, "bad length"
-assert len(bit2) == n, "bad length"
-
-# Get lightleaf data
-leaf1 = LightLeaf(bit1, letters)
-leaf2 = LightLeaf(bit2, letters)
-
-if leaf1.top != leaf2.top:
-	print "INCOMPATIBLE"
-	exit()
-
-# }}}1
-# Initialize vertices and edges {{{1
-vertices = [Vertex(i) for i in range(n)]
-for e in leaf1.edges:
-	a,b = sorted(e[1:3])
-	vertices[a].up = vertices[b]
-for e in leaf2.edges:
-	a,b = sorted(e[1:3])
-	vertices[a].down = vertices[b]
-# Find out where the top is divided
-# If I'm right, shouldn't matter whether leaf1 or leaf2 is used
-dividers = [vertices[x[1]] for x in leaf1.nodes_open]
-# }}}1
-# Determines connected components and cycles {{{1
-
-# Connected components
-def infiltrate(v, c):
-	# Colors everything connected to v with color c
-	#if v is None: return
-	assert type(v) != type(0), "fix your types"
-	if v.component == c: # Cycle detected, remember and quit
-		c.addBreakpoint(v)
-		return
-	assert v.component is None, "Houston, we have a problem."
-	v.component = c
-	for i in (v.up, v.down): # i is a vertex
-		if i is None: continue
-		infiltrate(i, c)
-		
-num_components = 0
-components = []
-for v in vertices:
-	if v.component is not None: continue # already done
-	if v.up is None and v.down is None and v in dividers: continue # this is a straight line
-	new_component = Component(num_components, v)
-	infiltrate(v, new_component) # flood fill
-	components.append(new_component)
-	new_component.makePockets() # construct the pockets for this component
-	num_components += 1
-
-
-# print [v.component_num for v in vertices]
-# print cycle_endpoints
-# }}}1
-# Feed things into pockets {{{1
-class Diagram(Pocket):
+class Universe(Pocket):
 	def __init__(self):
 		self.contents = []
 	def __repr__(self):
@@ -221,39 +157,101 @@ class Diagram(Pocket):
 		return True
 	def evaluate(self):
 		return prod([thing.evaluate() for thing in self.contents])
-universe = Diagram() # temp pointer
-for c in components:
-	universe.feed(c)
-# }}}1
-# Mark certain things as attached {{{1
-for c in components:
-	for p in c.pockets[:-1]:
-		p.attached = True
-	if c.breakpoints[-1] in dividers:
-		c.pockets[-1].attached = True
 
-# }}}
-# Split into regions by dividers and evaluate each region {{{1
-final_result = 1
-top_level_components = copy.copy(universe.contents)
-for d in reversed(dividers):
-	# Evaluate stuff in the region specified by the divider
-	for c in top_level_components:
-		if c.breakpoints[0] > d:
-			final_result *= c.evaluate()
-			top_level_components.remove(c)
-	# Push it through
-	if final_result != 0:
-		if d.color == 's':
-			final_result = final_result.subs(s = -alpha_s, t = alpha_t + coeff_x * alpha_s)
+def evaluateForm(letters, bit1, bit2):
+	# Get input {{{1
+	n = len(letters)
+	assert len(bit1) == n, "bad length"
+	assert len(bit2) == n, "bad length"
+
+	# Get lightleaf data
+	leaf1 = LightLeaf(bit1, letters)
+	leaf2 = LightLeaf(bit2, letters)
+
+	if leaf1.top != leaf2.top:
+		return "INCOMPATIBLE"
+
+	# }}}1
+	# Initialize vertices and edges {{{1
+	vertices = [Vertex(i, letters[i]) for i in range(n)]
+	for e in leaf1.edges:
+		a,b = sorted(e[1:3])
+		vertices[a].up = vertices[b]
+	for e in leaf2.edges:
+		a,b = sorted(e[1:3])
+		vertices[a].down = vertices[b]
+	# Find out where the top is divided
+	# If I'm right, shouldn't matter whether leaf1 or leaf2 is used
+	dividers = [vertices[x[1]] for x in leaf1.nodes_open]
+	# }}}1
+	# Determines connected components and cycles {{{1
+
+	# Connected components
+	def infiltrate(v, c):
+		# Colors everything connected to v with color c
+		#if v is None: return
+		assert type(v) != type(0), "fix your types"
+		if v.component == c: # Cycle detected, remember and quit
+			c.addBreakpoint(v)
+			return
 		else:
-			final_result = final_result.subs(t = -alpha_t, s = alpha_s + coeff_y * alpha_t)
-	else: break
-	divider_color = 't' if d.color == 's' else 't' # switch divider color
-else:
-	final_result *= prod([c.evaluate() for c in top_level_components]) # the rest of the components
-# }}}
+			assert v.component is None, "Houston, we have a problem."
+			v.component = c
+			c.members.append(v)
+		for i in (v.up, v.down): # i is a vertex
+			if i is None: continue
+			infiltrate(i, c)
+	num_components = 0
+	components = []
+	# look for each component to flood fill
+	for v in vertices:
+		if v.component is not None: continue # already added this vertex to some component
+		if v.up is None and v.down is None and v in dividers: continue # this is a straight line
+		new_component = Component(num_components, v)
+		infiltrate(v, new_component) # flood fill
+		components.append(new_component) # add to master list of components
+		new_component.makePockets() # construct the pockets for this component
+		num_components += 1
+	# }}}1
+	# Feed things into pockets {{{1
+	universe = Universe() # temp pointer
+	for c in components:
+		universe.feed(c)
+	# }}}1
+	# Mark certain things as attached {{{1
+	for c in components:
+		for p in c.pockets[:-1]:
+			p.attached = True
+		if c.breakpoints[-1] in dividers:
+			c.pockets[-1].attached = True
+
+	# }}}
+	# Split into regions by dividers and evaluate each region {{{1
+	final_result = 1
+	top_level_components = copy.copy(universe.contents)
+	for d in reversed(dividers):
+		# Evaluate stuff in the region specified by the divider
+		for c in top_level_components:
+			if c.breakpoints[0] > d:
+				final_result *= c.evaluate()
+				top_level_components.remove(c)
+		# Push it through
+		if final_result != 0:
+			if d.color == 's':
+				final_result = final_result.subs(s = -alpha_s, t = alpha_t + coeff_x * alpha_s)
+			else:
+				final_result = final_result.subs(t = -alpha_t, s = alpha_s + coeff_y * alpha_t)
+		else: break
+	else:
+		final_result *= prod([c.evaluate() for c in top_level_components]) # the rest of the components
+	return final_result
+	# }}}
 
 if __name__ == "__main__":
-	print final_result
+	f = sys.stdin
+	print  evaluateForm(
+		f.readline().strip(), 
+		f.readline().strip(), 
+		f.readline().strip(),
+		)
 # vim: fdm=marker
